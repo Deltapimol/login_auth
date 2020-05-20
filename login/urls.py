06fb14ce5defed98bb4dd08d537496cb1cff0ac2
Deltapimol <43440596+Deltapimol@users.noreply.github.com>
2020-05-21 00:40:39 +0530

    User creation with login and logout functionality success

--- login/urls.py
+++ login/urls.py
@@ -2,6 +2,10 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('',views.index),
-    path('home/',views.home)
+
+    path('',views.index, name='index'),
+    path('home/',views.home, name='home'),
+    path('accounts/',include('django.contrib.auth.urls')),
+    path('register/',views.register, name='register')
+    
 ]
--- login/views.py
+++ login/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
-from django.contrib import auth
-from django.forms import forms
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
 
 def index(request):
     context = { 
@@ -12,3 +12,22 @@ def index(request):
 def home(request):
     
     return HttpResponse(content=b'Hello! This is home page.')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username,password=password)
+            login(request, user)
+            return redirect('index')
+        
+    else:
+        form = UserCreationForm()
+    context = { 'form': form }
+    return render(request, 'registration/register.html',context)
+
+
+
