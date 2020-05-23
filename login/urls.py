465fee93123066e35bfe7ca8129b2d6132f41700
Deltapimol <43440596+Deltapimol@users.noreply.github.com>
2020-05-23 23:36:40 +0530

    Create custom form class derived from UserCreationForm

--- login/urls.py
+++ login/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('',views.index, name='index'),
     path('home/',views.home, name='home'),
     path('accounts/',include('django.contrib.auth.urls')),
-    path('register/',views.register, name='register')
+    path('register/',views.register, name='register'),
+    path('userform/',views.userform,name='userform'),
+    path('customuser/',views.customuser,name='customuser')
     
 ]
--- login/views.py
+++ login/views.py
@@ -2,15 +2,55 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
+from django import forms
 
-def index(request):
-    context = { 
-                "users" : [1,2,4]
-    }
-    return render(request,'index.html', context)
+class CustomForm(UserCreationForm):
 
-def home(request):
+    firstname = forms.CharField(label='First name')
+    lastname = forms.CharField(label='Last name')
+    email = forms.EmailField(label='Email')
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model=User
+        fields = ('firstname','lastname','email')
     
+    def save(self,commit=True):
+        user = super(CustomForm,self).save(commit=False)
+        user.firstname = self.cleaned_data['firstname']
+        user.lastname = self.cleaned_data['lastname']
+        user.email = self.cleaned_data['email']
+        user.password = self.cleaned_data['password']
+        if commit:
+            user.save()
+        return user
+
+def userform(request):
+    if request.method == 'POST':
+        form = CustomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            name1 = form.cleaned_data['firstname']
+            name2 = form.cleaned_data['lastname'] 
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            user = authenticate(email=email,password=password)
+            login(request,user)
+            return redirect('customuser')
+    else:
+        form = CustomForm()
+    context = { 'form': form }
+    return render(request,'registration/userform.html',context)
+
+def customuser(request):
+    context = {'user' : request.user}
+    return render(request,'registration/customuser.html',context)
+            
+def index(request):
+    return render(request,'index.html')
+
+def home(request):  
     return HttpResponse(content=b'Hello! This is home page.')
 
 def register(request):
@@ -28,6 +68,15 @@ def register(request):
         form = UserCreationForm()
     context = { 'form': form }
     return render(request, 'registration/register.html',context)
+
+
+
+
+
+
+
+    
+
 
 
 
